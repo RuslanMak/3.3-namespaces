33cc46fdb5a862b2f7681d48b2abfed82e33b532
RuslanMak <makarovemd@gmail.com>
2018-05-03 12:05:48 +0300

    version-1.4

--- autoload.php
+++ autoload.php
@@ -16,22 +16,5 @@ function myAutoloader($className)
     }
 }
 
-//function myAutoloaderForInterface($className)
-//{
-//    $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
-//
-//    $filePath = $className . '.class.php';
-//    $filePathtoInterface = $className . '.php';
-//
-//    if (file_exists($filePath)) {
-//        include "$filePath";
-//    } elseif (file_exists($filePathtoInterface)) {
-//        include "$filePathtoInterface";
-//    }  else {
-//        die("Класс $className не существует");
-//    }
-//}
-
 spl_autoload_register('myAutoloader');
-spl_autoload_register('myAutoloader'); //второй раз для загрузки интерфейса. Не вижу смысла создавать вторую автозагрузку
-//spl_autoload_register('myAutoloaderForInterface');
+spl_autoload_register('myAutoloader'); //второй раз - для загрузки интерфейса. Не вижу смысла создавать вторую ф-цию автозагрузкы
